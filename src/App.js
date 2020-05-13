d391bde3a747565a58a7d58cd04428a200918afa
unknown <wendellwoneys@gmail.com>
2020-05-13 20:05:11 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/header';
+import Footer from './components/footer';
 
 import './assets/css/global.css';
 
 function App() {
   return ( 
-    <Header/>
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
    );
 }
 
---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer-conteiner">
+            <span>Copyright Designed by Wendell Woney. All rights reserved.</span>
+        </footer>
+    );
+}
